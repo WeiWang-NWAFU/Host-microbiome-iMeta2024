c3416ecb22f7d7d27d0067f65badbd0c823b6890
WeiWang-NWAFU <wangw369@nwafu.edu.cn>
2024-10-16 12:33:59 +0800

    uploadfile

--- Scripts/TWAS/qqman.R
+++ Scripts/TWAS/qqman.R
@@ -6,8 +6,8 @@ twas <-read.csv(file="C:/Users/data.csv", header=T)
 ##plot
 colorset<-c("#B395BD", "#EA8379", "#7DAEE0")
 
-manhattan(twas, p cex.lab = 1.5, cex.main = 2,cex = 1.2,
+manhattan(twas, cex.lab = 1.5, cex.main = 2,cex = 1.2,
             chr = "CHR", bp = "BP", snp= "SNP", p = "P",
-            suggestiveline = -log10(p), genomewideline = -log10(p) , annotateTop = FALSE,
+            suggestiveline = -log10(6.0e-05), genomewideline = -log10(3.0e-06) , annotateTop = FALSE,
             col = colorset)
   
